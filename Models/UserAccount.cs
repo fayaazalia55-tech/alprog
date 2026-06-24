4de82e400c05483bd924ae9b8fc90fd9c57afbfe
fayaazalia55-tech <fayaazalia55@gmail.com>
2026-06-24 19:47:57 +0700

    Add files via upload

---
+++ Models/UserAccount.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+namespace EvacuationDashboard.Models
+{
+    [Table("UserAccount")]
+    public class UserAccount
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; } // Nomor urut otomatis, akan dipakai sebagai ID unik oleh OpenCV
+
+        [Unique, NotNull]
+        public string Username { get; set; } = string.Empty; // Input teks nama untuk login [cite: 125, 143]
+
+        [NotNull]
+        public string Password { get; set; } = string.Empty; // Kata sandi pengguna [cite: 125, 143]
+
+        [NotNull]
+        public string Role { get; set; } = string.Empty; // Berisi string "Maintenance" atau "Building Manager" [cite: 125, 143]
+
+        [NotNull]
+        public string PhotoPath { get; set; } = string.Empty; // Lokasi penyimpanan file gambar "1_ID_Nama.jpg" di laptop
+    }
+}
