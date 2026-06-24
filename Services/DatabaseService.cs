17c9d58964fe36f4bbde3b73d6a954f6d9a98976
fayaazalia55-tech <fayaazalia55@gmail.com>
2026-06-24 19:46:10 +0700

    Add files via upload

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using EvacuationDashboard.Models;
+using EvacuationDashboard.Models;
+using SQLite;
+
+namespace EvacuationDashboard.Services
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection _connection;
+
+        public DatabaseService()
+        {
+            // Membuat atau membuka file database bernama "EvakuasiGedung.db3" di folder aman lokal laptop
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "EvakuasiGedung.db3");
+            _connection = new SQLiteAsyncConnection(dbPath);
+
+            // Otomatis membuat tabel UserAccount jika tabel tersebut belum ada di komputer
+            _connection.CreateTableAsync<UserAccount>().Wait();
+        }
+
+        public SQLiteAsyncConnection GetConnection()
+        {
+            return _connection;
+        }
+    }
+}
